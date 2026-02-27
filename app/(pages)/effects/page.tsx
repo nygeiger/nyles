d0a3a6c85b6de8ba28c4cdcab93c6289c52911cb
Nyles Geiger <nygeiger@gmail.com>
2026-02-27 00:35:19 -0500

    Initial. Migration of tic tac toe project. Add ability to select different "effects"

--- app/(pages)/effects/page.tsx
+++ app/(pages)/effects/page.tsx
@@ -1,17 +1,44 @@
+'use client'
+
 import GameOfLifeComponent from '@/app/components/effects/GameOfLife';
+import TicTacToe2 from '@/app/components/effects/TicTacToe';
+import { ChangeEvent, JSX, useRef, useState } from 'react';
+
+const EffectDict = {
+  GAME_OF_LIFE: "Game of life",
+  TIC_TAC_TOE: "Tic Tac Toe"
+}
+
+const effectComponentMap: Record<string, JSX.Element> = {
+  [EffectDict.GAME_OF_LIFE]: <GameOfLifeComponent />,
+  [EffectDict.TIC_TAC_TOE]: <TicTacToe2 />,
+};
+
+
 
 export default function EffectsPage() {
+  const [currentEffect, setCurrentEffect] = useState<string>(EffectDict.GAME_OF_LIFE);
+
+  const effectComponent = effectComponentMap[currentEffect] ?? <></>
+
   return (
     <section className="flex-auto items-center h-fit w-full">
       <h1 className="mb-4 text-3xl font-bold tracking-tighter">
         Effects & Visualizations
       </h1>
-      <p className="mb-8 text-gray-600 pb-6">
+
+      <p className="text-gray-600 pb-4">
         A collection of interactive effects and visualizations to explore.
       </p>
 
+      Current Effect: <select id="effectSelect" className="animate-pulse" style={{WebkitAppearance: "menulist-button"}} onChange={(e) => setCurrentEffect(e.currentTarget.value)}>
+        {Object.values(EffectDict).map((el, i) => {
+          return <option key={i} value={el}>{el}</option>
+        })}
+      </select>
+
       <div className="grid gap-8 my-8 bg-black">
-        <GameOfLifeComponent />
+        {effectComponent}
       </div>
 
     </section>
